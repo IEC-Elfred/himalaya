1a548d9b257a28818cc41bb2c80d835c64845cec
IEC-Elfred <854000628@qq.com>
2022-01-25 17:41:27 +0800

    completed player callback

--- app/src/main/java/com/uniqueAndroid/ximalaya/interfaces/IPlayerCallback.java
+++ app/src/main/java/com/uniqueAndroid/ximalaya/interfaces/IPlayerCallback.java
@@ -1,4 +1,46 @@
 package com.uniqueAndroid.ximalaya.interfaces;
 
-public class IPlayerCallback {
+import android.os.Trace;
+
+import com.ximalaya.ting.android.opensdk.model.track.Track;
+import com.ximalaya.ting.android.opensdk.player.service.XmPlayListControl;
+
+import java.util.List;
+
+public interface IPlayerCallback {
+
+    void onPlayStart();
+
+    void onPlayPause();
+
+    void onPlayStop();
+
+    void onPlayError();
+
+    void nextPlay(Track track);
+
+    void onPrePlay(Track track);
+
+    /**
+     * 播放列表数据加载完成
+     *
+     * @param list
+     */
+    void onListLoaded(List<Trace> list);
+
+    /**
+     * 播放器模式改变
+     *
+     * @param playMode
+     */
+    void onPlayModeChange(XmPlayListControl.PlayMode playMode);
+
+    void onProgressChange(long currentProgress, long total);
+
+    /**
+     * 广告加载
+     */
+    void onAdLoading();
+
+    void onAdFinished();
 }
