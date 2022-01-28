c46f0f1e1445c5616298d6e53ff49b3bebc94020
IEC-Elfred <854000628@qq.com>
2022-01-29 02:14:39 +0800

    add mainactivity callback

--- app/src/main/java/com/uniqueAndroid/ximalaya/MainActivity.java
+++ app/src/main/java/com/uniqueAndroid/ximalaya/MainActivity.java
@@ -1,6 +1,8 @@
 package com.uniqueAndroid.ximalaya;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
@@ -9,23 +11,42 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.uniqueAndroid.ximalaya.adapters.IndicatorAdapter;
 import com.uniqueAndroid.ximalaya.adapters.MainContentAdapter;
+import com.uniqueAndroid.ximalaya.interfaces.IPlayerCallback;
+import com.uniqueAndroid.ximalaya.presenters.PlayerPresenter;
 import com.uniqueAndroid.ximalaya.utils.LogUtil;
+import com.uniqueAndroid.ximalaya.views.RoundRectImageView;
+import com.ximalaya.ting.android.opensdk.model.track.Track;
+import com.ximalaya.ting.android.opensdk.player.service.XmPlayListControl;
 
 import net.lucode.hackware.magicindicator.MagicIndicator;
 import net.lucode.hackware.magicindicator.ViewPagerHelper;
 import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
 
-public class MainActivity extends FragmentActivity {
-    private static final String TAG = "categories";
+import java.util.List;
+
+public class MainActivity extends FragmentActivity implements IPlayerCallback {
+    private static final String TAG = "MainActivity";
     private MagicIndicator indicator;
     private ViewPager contentViewPager;
     private IndicatorAdapter indicatorAdapter;
+    private RoundRectImageView mRoundRectImageView;
+    private TextView mHeaderTitle;
+    private TextView mSubTitle;
+    private ImageView mPlayControl;
+    private PlayerPresenter mPlayerPresenter;
 
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initEvent();
+        initPresenter();
+    }
+
+    private void initPresenter() {
+        mPlayerPresenter = PlayerPresenter.getPlayerPresenter();
+        mPlayerPresenter.registerViewCallback(this);
     }
 
     public void initEvent() {
@@ -43,7 +64,6 @@ public class MainActivity extends FragmentActivity {
     private void initView() {
         indicator = this.findViewById(R.id.main_indicator);
         indicator.setBackgroundColor(this.getResources().getColor(R.color.main_color));
-        //创建indicator的适配器
         indicatorAdapter = new IndicatorAdapter(this);
         CommonNavigator commonNavigator = new CommonNavigator((this));
         commonNavigator.setAdjustMode(true);
@@ -58,5 +78,92 @@ public class MainActivity extends FragmentActivity {
         contentViewPager.setAdapter(mainContentAdapter);
         indicator.setNavigator(commonNavigator);
         ViewPagerHelper.bind(indicator, contentViewPager);
+
+        //bottom播放控制相关
+        mRoundRectImageView = this.findViewById(R.id.main_track_cover);
+        mHeaderTitle = this.findViewById(R.id.main_title);
+        mSubTitle = this.findViewById(R.id.main_sub_title);
+        mPlayControl = this.findViewById(R.id.main_play_control);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPlayerPresenter != null) {
+            mPlayerPresenter.unRegisterViewCallback(this);
+        }
+    }
+
+    @Override
+    public void onPlayStart() {
+
+    }
+
+    @Override
+    public void onPlayPause() {
+
+    }
+
+    @Override
+    public void onPlayStop() {
+
+    }
+
+    @Override
+    public void onPlayError() {
+
+    }
+
+    @Override
+    public void nextPlay(Track track) {
+
+    }
+
+    @Override
+    public void onPrePlay(Track track) {
+
+    }
+
+    @Override
+    public void onListLoaded(List<Track> list) {
+
+    }
+
+    @Override
+    public void onPlayModeChange(XmPlayListControl.PlayMode playMode) {
+
+    }
+
+    @Override
+    public void onProgressChange(long currentProgress, long total) {
+
+    }
+
+    @Override
+    public void onAdLoading() {
+
+    }
+
+    @Override
+    public void onAdFinished() {
+
+    }
+
+    @Override
+    public void onTrackUpdate(Track track, int playIndex) {
+        if (track != null ) {
+            String trackTitle = track.getTrackTitle();
+            String nickname = track.getAnnouncer().getNickname();
+            String coverUrlMiddle = track.getCoverUrlMiddle();
+            LogUtil.d(TAG,"trackTitle ---->" + trackTitle);
+            LogUtil.d(TAG,"nickname ---->" + nickname);
+            LogUtil.d(TAG,"coverUrlMiddle ---->" + coverUrlMiddle);
+
+        }
+    }
+
+    @Override
+    public void updateListOrder(boolean isReverse) {
+
     }
 }
